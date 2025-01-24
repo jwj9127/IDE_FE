dfeb9d27e44b447baa4a8ce00bd4eebc89e8aae4
jiwonee02 <nepthun@naver.com>
2025-01-24 16:06:39 +0900

    헤더 css 변경

--- codable/src/components/Header/header.tsx
+++ codable/src/components/Header/header.tsx
@@ -1,9 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./header.scss";
 import logo from "../../assets/logo.png";
 
 const Header: React.FC = () => {
+    const location = useLocation();
+
+    // 현재 라우터 경로에 따라 버튼 렌더링
+    const renderNavContent = () => {
+        if (location.pathname === "/editor") {
+            return (
+                <button
+                    className="nav-button"
+                    onClick={() => window.history.back()}
+                >
+                    나가기
+                </button>
+            );
+        } else if (
+            location.pathname === "/chat" ||
+            location.pathname === "/study"
+        ) {
+            return (
+                <button
+                    className="nav-button"
+                    onClick={() => console.log("로그아웃")}
+                >
+                    로그아웃
+                </button>
+            );
+        }
+        return null;
+    };
+
     return (
         <header className="header">
             <div className="logo">
@@ -11,7 +40,7 @@ const Header: React.FC = () => {
                     <img src={logo} alt="Codable" className="logo-image" />
                 </Link>
             </div>
-            <nav className="nav"></nav>
+            <nav className="nav">{renderNavContent()}</nav>
         </header>
     );
 };
