a2769f3e5ad7d442666e4aebaf58bfd4f04b01f9
jwj9127 <jwj9127@naver.com>
2025-01-27 23:32:19 +0900

    ✨ [기능 추가]
    헤더에서 토큰 가져오기

--- codable/src/pages/Login/Login.tsx
+++ codable/src/pages/Login/Login.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import loginImage1 from "../../assets/loginPage1.png";
 import loginImage2 from "../../assets/loginPage2.png";
 
 import styles from "./Login.module.scss";
 
 const Login: React.FC = () => {
+  const location = useLocation();
   const link = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code`;
+
+  useEffect(() => {
+    const query = new URLSearchParams(location.search);
+    const token = query.get("token");
+    if (token) {
+      localStorage.setItem("authToken", token);
+      console.log(token);
+    }
+  }, [location.search]);
 
   const loginHandler = () => {
     window.location.href = link;
