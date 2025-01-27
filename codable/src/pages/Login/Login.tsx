f3081fe5adf08f98a5fcddeed9e49c0d667817b9
minzix <minzikx@sookmyung.ac.kr>
2025-01-27 22:45:29 +0900

    🎨 로그인 페이지 수정
    //

--- codable/src/pages/Login/Login.tsx
+++ codable/src/pages/Login/Login.tsx
@@ -2,25 +2,26 @@ import React from "react";
 import loginImage1 from "../../assets/loginPage1.png";
 import loginImage2 from "../../assets/loginPage2.png";
 
+import styles from "./Login.module.scss";
+
 const Login: React.FC = () => {
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code`;
 
-    const link = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code`;
+  const loginHandler = () => {
+    window.location.href = link;
+  };
 
-    const loginHandler = () => {
-        window.location.href = link;
-      };
-
-      return (
-        <div className="frame">
-          <div className="innerFrame">
-            <img src = {loginImage1} />
-            <img src = {loginImage2} />
-          </div>
-          <button type='button' onClick={loginHandler}>
-          카카오톡으로 시작하기
-          </button>
-        </div>
-      );
+  return (
+    <div className={styles.loginContainer}>
+      <div className={styles.background}>
+        <img src={loginImage1} alt="Background" className={styles.backgroundImage} />
+        <img src={loginImage2} alt="Background" className={styles.backgroundImage} />
+      </div>
+      <button type="button" onClick={loginHandler} className={styles.kakaoButton}>
+        카카오톡으로 시작하기
+      </button>
+    </div>
+  );
 };
 
 export default Login;
