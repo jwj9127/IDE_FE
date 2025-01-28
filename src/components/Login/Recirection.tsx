a30628418c66717e5fa212daefa806e173ada2e2
jwj9127 <jwj9127@naver.com>
2025-01-29 00:58:30 +0900

    🐛 [fix] url 수정
    - url 수정합니다

--- src/components/Login/Recirection.tsx
+++ src/components/Login/Recirection.tsx
@@ -49,7 +49,7 @@ const Recirection: React.FC = () => {
     if (id !== null && name !== null && email !== null) {
       axios({
         method: "post",
-        url: "/api/login/kakao",
+        url: "https://codeable.duckdns.org/api/login/kakao",
         data: kakaoUserInfo,
       }).then((result) => {
         if (result.data.code === 201) {
