9cedb3c4aebf4ecbca8be286f6ad6903f29472b3
jwj9127 <jwj9127@naver.com>
2025-01-29 15:13:20 +0900

    [feat]: use hook 추가

--- src/components/Login/Recirection.tsx
+++ src/components/Login/Recirection.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { KakaoUserInfoRequestDto } from "./KakaoUserInfoRequestDto";
 
 const Recirection: React.FC = () => {
@@ -9,56 +9,58 @@ const Recirection: React.FC = () => {
   const [name, setName] = useState<any>(null);
   const [email, setEmail] = useState<any>(null);
 
-  axios({
-    method: "post",
-    url: "https://kauth.kakao.com/oauth/token",
-    data: {
-      grant_type: "authorization_code",
-      client_id: "e219a9b5f8b7dd45ffd5e20765d715de",
-      redirect_uri: `${process.env.REACT_APP_REDIRECT_URI}`,
-      code: `${code}`,
-      client_secret: "vMIh6B4uKHJhQkGiCLQgBLfjJZ7jcJVw",
-    },
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-    },
-  }).then((result) => {
-    setAccessToken(result.data.access_token);
-  });
-
-  if (accessToken !== null) {
+  useEffect(() => {
     axios({
-      method: "get",
-      url: " https://kapi.kakao.com/v2/user/me",
+      method: "post",
+      url: "https://kauth.kakao.com/oauth/token",
+      data: {
+        grant_type: "authorization_code",
+        client_id: "e219a9b5f8b7dd45ffd5e20765d715de",
+        redirect_uri: `${process.env.REACT_APP_REDIRECT_URI}`,
+        code: `${code}`,
+        client_secret: "vMIh6B4uKHJhQkGiCLQgBLfjJZ7jcJVw",
+      },
       headers: {
-        Authorization: `Bearer ${accessToken}`,
         "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
       },
     }).then((result) => {
-      setId(result.data.id);
-      setName(result.data.properties.nickname);
-      setEmail(result.data.kakao_account.email);
+      setAccessToken(result.data.access_token);
     });
-
-    const kakaoUserInfo: KakaoUserInfoRequestDto = {
-      id,
-      name,
-      email,
-    };
-
-    if (id !== null && name !== null && email !== null) {
+  
+    if (accessToken !== null) {
       axios({
-        method: "post",
-        url: `https://${process.env.REACT_APP_BASE_URL}/api/login/kakao`,
-        data: kakaoUserInfo,
-        withCredentials: true,
+        method: "get",
+        url: " https://kapi.kakao.com/v2/user/me",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
       }).then((result) => {
-        if (result.data.code === 201) {
-          localStorage.setItem("token", result.data.data.jwtToken);
-        }
+        setId(result.data.id);
+        setName(result.data.properties.nickname);
+        setEmail(result.data.kakao_account.email);
       });
+  
+      const kakaoUserInfo: KakaoUserInfoRequestDto = {
+        id,
+        name,
+        email,
+      };
+      
+      if (id !== null && name !== null && email !== null) {
+        axios({
+          method: "post",
+          url: `https://${process.env.REACT_APP_BASE_URL}/api/login/kakao`,
+          data: kakaoUserInfo,
+          withCredentials: true,
+        }).then((result) => {
+          if (result.data.code === 201) {
+            localStorage.setItem("token", result.data.data.jwtToken);
+          }
+        });
+      }
     }
-  }
+  },[code])
 
   return <></>;
 };
