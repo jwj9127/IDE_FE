a253796eefee16fd3caa25d163ad6f15e11bea7a
jwj9127 <jwj9127@naver.com>
2025-01-30 02:07:59 +0900

    🐛 [fix] wss 설정
    wss 설정으로 fix했습니다

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -7,10 +7,7 @@ import "./Chat.scss";
 
 const Chat: React.FC = () => {
   const [stompClient, setStompClient] = useState<any>(null);
-  const baseURL =
-    window.location.protocol === "https:"
-      ? `wss://${process.env.REACT_APP_BASE_URL}/ws/chat`
-      : `ws://${process.env.REACT_APP_BASE_URL}/ws/chat`;
+  const baseURL = `wss://${process.env.REACT_APP_BASE_URL}/ws/chat`;
 
   // 채팅 메시지 상태 (닉네임과 텍스트를 저장)
   const [messages, setMessages] = useState<{ user: string; text: string }[]>(
