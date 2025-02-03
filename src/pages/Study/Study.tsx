24148a19b5d024b46387b7723a69e54261674bf5
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-02-03 09:23:57 +0900

    달력 날짜 불일치 이슈 수정

--- src/pages/Study/Study.tsx
+++ src/pages/Study/Study.tsx
@@ -59,7 +59,8 @@ const Study = () => {
     const now = new Date();
     const year = now.getFullYear();
     const month = now.getMonth();
-    const firstDay = new Date(year, month, 1).getDay();
+    // const firstDay = new Date(year, month, 1).getDay();
+    const firstDay = (new Date(year, month, 1).getDay() + 6) % 7;
     const lastDate = new Date(year, month + 1, 0).getDate();
 
     const calendar = Array(firstDay)
